128eb7a627332f7bf84d7f595041718ed8ad5488
Ben <ben.marsh304@gmail.com>
2023-07-31 07:35:40 -0700

    app,py reverted to V1 bot.

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 import streamlit as st
 import sqlite3
-from langchain.agents import ZeroShotAgent, create_sql_agent, AgentExecutor
+from langchain.agents import create_sql_agent
 from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 from langchain.sql_database import SQLDatabase
 from langchain.llms.openai import OpenAI
-from langchain.chat_models.openai import ChatOpenAI
-from langchain.agents.agent_types import AgentType
-from langchain.memory import ConversationBufferMemory
-from langchain import SQLDatabaseChain
 
 
 # App Title and Page Config
@@ -24,68 +20,11 @@ conn = sqlite3.connect('chatbot_database.db')
 db = SQLDatabase.from_uri("sqlite:///./chatbot_database.db")
 toolkit = SQLDatabaseToolkit(db=db, llm=OpenAI(temperature=0))
 
-prefix = """You are an agent designed to interact with a SQL database.
-Given an input question, create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
-Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most {top_k} results.
-You can order the results by a relevant column to return the most interesting examples in the database.
-Never query for all the columns from a specific table, only ask for the relevant columns given the question.
-You have access to tools for interacting with the database.
-Only use the below tools. Only use the information returned by the below tools to construct your final answer.
-You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
-
-DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
-
-If the question does not seem related to the database, just return "I don\'t know" as the answer.
-"""
-suffix = """Begin!"
-
-Relevant pieces of previous conversation:
-{history}
-(You do not need to use these pieces of information if not relevant)
-
-Question: {input}
-Thought: I should look at the tables in the database to see what I can query.  Then I should query the schema of the most relevant tables.
-{agent_scratchpad}
-"""
-
-prompt = ZeroShotAgent.create_prompt(
-    tools=toolkit,
-    prefix=prefix,
-    suffix=suffix,
-    input_variables=["input", "chat_history", "agent_scratchpad"],
+agent_executor = create_sql_agent(
+    llm=OpenAI(temperature=0),
+    toolkit=toolkit,
+    verbose=True
 )
-memory = ConversationBufferMemory(memory_key="chat_history")
-llm_chain = SQLDatabaseChain(llm=OpenAI(temperature=0), prompt=prompt)
-agent = ZeroShotAgent(llm_chain=llm_chain, tools=toolkit, verbose=True)
-agent_chain = AgentExecutor.from_agent_and_tools(
-    agent=agent, tools=toolkit, verbose=True, memory=memory
-)
-# agent_executor = create_sql_agent(
-#     llm=OpenAI(temperature=0),
-#     toolkit=toolkit,
-#     verbose=True
-# )
-# memory = ConversationBufferMemory(memory_key = 'history' , input_key = 'input')
-# llm = ChatOpenAI(model_name = 'gpt-3.5-turbo' , temperature = 0)
-# suffix = """Begin!
-
-# Relevant pieces of previous conversation:
-# {history}
-# (You do not need to use these pieces of information if not relevant)
-
-# Question: {input}
-# Thought: I should look at the tables in the database to see what I can query.  Then I should query the schema of the most relevant tables.
-# {agent_scratchpad}
-# """
-# agent_executor = create_sql_agent(
-#     llm = llm,
-#     toolkit = SQLDatabaseToolkit(db=db, llm=llm),
-#     agent_type = AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-#     input_variables = ["input", "agent_scratchpad","history"],
-#     suffix = suffix, # must have history as variable,
-#     agent_executor_kwargs = {'memory':memory}
-# )
-
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
@@ -108,7 +47,7 @@ def generate_response(prompt_input):
             string_dialogue += "User: " + dict_message["content"] + "\\n\\n"
         else:
             string_dialogue += "Assistant: " + dict_message["content"] + "\\n\\n"
-    output = agent_chain.run(prompt_input)
+    output = agent_executor.run(prompt_input)
     return output
 
 # User-provided prompt
