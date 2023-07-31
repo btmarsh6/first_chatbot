217e4b84ebb60826071514a8389ef44ea522a12e
Ben <ben.marsh304@gmail.com>
2023-07-31 08:16:28 -0700

    Memory debugging.

--- app_v2.py
+++ app_v2.py
@@ -26,24 +26,10 @@ toolkit = SQLDatabaseToolkit(db=db, llm=OpenAI(temperature=0))
 
 memory = ConversationBufferMemory(memory_key="chat_history")
 
-prefix = """You are an agent designed to interact with a SQL database.
-Given an input question, create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
-Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most {top_k} results.
-You can order the results by a relevant column to return the most interesting examples in the database.
-Never query for all the columns from a specific table, only ask for the relevant columns given the question.
-You have access to tools for interacting with the database.
-Only use the below tools. Only use the information returned by the below tools to construct your final answer.
-You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
-
-DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
-
-If the question does not seem related to the database, just return "I don\'t know" as the answer.
-"""
-
 suffix = """Begin!"
 
 Relevant pieces of previous conversation:
-{history}
+{chat_history}
 (You do not need to use these pieces of information if not relevant)
 
 Question: {input}
@@ -51,19 +37,15 @@ Thought: I should look at the tables in the database to see what I can query.  T
 {agent_scratchpad}
 """
 
-prompt = ZeroShotAgent.create_prompt(
-    tools=toolkit,
-    prefix=prefix,
-    suffix=suffix,
-    input_variables=["input", "chat_history", "agent_scratchpad"],
-)
 agent_executor = create_sql_agent(
     llm=OpenAI(temperature=0),
     toolkit=toolkit,
     verbose=True,
-    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    suffix=suffix,
+    input_variables= ['chat_history', 'input', 'agent_scratchpad'],
     agent_executor_kwargs={'memory':memory}
 )
+
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
